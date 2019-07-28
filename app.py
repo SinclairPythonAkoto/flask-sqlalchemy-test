a54eb11b55c928994975e6cbaf9ff7056bcf49ec
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-07-28 16:24:28 +0100

    new update

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import os
+import psycopg2
+
 from flask import Flask, render_template, url_for
 from flask sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://swowgedxgmeugi:0d1eda082de1fae25d151bcf290377d6c919c1b39f65f14a96f0982a194a0db3@ec2-174-129-41-127.compute-1.amazonaws.com:5432/d5t458ucvgs05j'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
 db = SQLAlchemy(app)
 
 class Data_Base(db.Model):
